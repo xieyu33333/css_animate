b4dc65ac6ef88d3c04c0ff6769d0eeccf9eec633
xieyu33333 <xieyu33333@gmail.com>
2014-05-05 17:17:51 +0800

    添加沿X轴翻转和2D翻转，并更新文档

--- ani.js
+++ ani.js
@@ -3,7 +3,7 @@
     direction: "+",
     speed: 100
   }
-  $.fn.ani_scroll = function(options){
+  $.fn.auto_rotate = function(options){
     var options = $.extend({}, defaults, options);
     if (options.direction == "-"){
       $(this).addClass("rotate_revert");
@@ -19,25 +19,49 @@
   }
 })(jQuery);
 
+
 (function($) {
   var defaults = { 
     deg: 180,
     speed: 100
   }
-
   function increment_deg(){ 
     var deg = 0
     return function(n){return deg+=n;}
   }
+  function increment_x_deg(){ 
+    var deg_x = 0
+    return function(n){return deg_x+=n;}
+  }
+  function increment_y_deg(){ 
+    var deg_y = 0
+    return function(n){return deg_y+=n;}
+  }
   var degs = increment_deg();
-  $.fn.rotate_y = function(options){
-    var options = $.extend({}, defaults, options);
-    $(this).css({
+  var degs_x = increment_x_deg();
+  var degs_y = increment_y_deg();
+
+  function transition(obj, options){
+    obj.css({
       "transition": "transform "+100/options.speed+"s",
       "-webkit-transition": "-webkit-transform "+100/options.speed+"s",
       "-moz-transition": "-moz-transform "+100/options.speed+"s",
       "-o-transition": "-o-transform "+100/options.speed+"s"
     });
-    $(this).css("transform", "rotateY("+degs(options.deg)+"deg)");
   }
-})(jQuery)
+  $.fn.rotate = function(options){
+    var options = $.extend({}, defaults, options);
+    transition($(this), options);
+    $(this).css("transform", "rotate("+degs(options.deg)+"deg)");
+  }
+  $.fn.rotate_x = function(options){
+    var options = $.extend({}, defaults, options);
+    transition($(this), options);
+    $(this).css("transform", "rotateX("+degs_x(options.deg)+"deg)");
+  }
+  $.fn.rotate_y = function(options){
+    var options = $.extend({}, defaults, options);
+    transition($(this), options);
+    $(this).css("transform", "rotateY("+degs_y(options.deg)+"deg)");
+  }
+})(jQuery);
